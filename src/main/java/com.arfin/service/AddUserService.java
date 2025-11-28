3b6262ca5a7e3580d696d4dd1054d038723579f9
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-11-27 22:45:59 -0300

    feat: Add initial model and service to user.

---
+++ src/main/java/com.arfin/service/AddUserService.java
@@ -0,0 +1,12 @@
+package com.arfin.service;
+
+import org.springframework.stereotype.Service;
+
+import com.arfin.model.User;
+
+@Service
+public class AddUserService {
+  public static void add(User user) {
+
+  }
+}
